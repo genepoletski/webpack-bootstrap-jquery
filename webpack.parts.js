e9475eb8f67a3334e442aba3c7c43bdf6b4d2b52
eugene poletski <web.developer.poletski@gmail.com>
2016-12-26 16:36:05 +0300

    Create webpack.parts.js

---
+++ webpack.parts.js
@@ -0,0 +1,196 @@
+const webpack = require('webpack');
+const path = require('path');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const PurifyCSSPlugin = require('purifycss-webpack-plugin');
+
+exports.devServer = function(options) {
+  return {
+    devServer: {
+      watchOptions: {
+        // Delay the rebuild after the first change
+        aggregateTimeout: 300,
+        // Poll using interval (in ms, accepts boolean too)
+        poll: 1000
+      },
+      // Enable history API fallback so HTML5 History API based
+      // routing works. This is a good default that will come
+      // in handy in more complicated setups.
+      historyApiFallback: true,
+
+      // Unlike the cli flag, this doesn't set
+      // HotModuleReplacementPlugin!
+      hot: true,
+      inline: true,
+
+      // Display only errors to reduce the amount of output.
+      stats: 'errors-only',
+
+      // Parse host and port from env to allow customization.
+      //
+      // If you use Vagrant or Cloud9, set
+      // host: options.host || '0.0.0.0';
+      //
+      // 0.0.0.0 is available to all network devices
+      // unlike default `localhost`.
+      host: options.host, // Defaults to `localhost`
+      port: options.port // Defaults to 8080
+    },
+    plugins: [
+      // ignore node_modules so CPU usage with poll watching drops significantly
+      new webpack.WatchIgnorePlugin([
+        path.join(__dirname, 'node_modules')
+      ]),
+      // Enable multi-pass compilation for enhanced performance
+      // in larger projects. Good default.
+      new webpack.HotModuleReplacementPlugin({
+        // Disabled as this won't work with html-webpack-template yet
+        //multiStep: true
+      })
+    ]
+  };
+};
+
+exports.setupCSS = function(paths) {
+  return {
+    module: {
+      rules: [
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader'],
+          include: paths
+        },
+        {
+          test: /\.scss$/,
+          use: ['style-loader', 'css-loader', 'sass-loader'],
+          include: paths
+        }
+      ]
+    }
+  };
+};
+
+exports.minify = function() {
+  return {
+    plugins: [
+      new webpack.optimize.UglifyJsPlugin({
+        // Don't beautify output (enable for neater output)
+        beautify: false,
+
+        // Eliminate comments
+        comments: false,
+
+        // Compression specific options
+        compress: {
+          warnings: false,
+
+          // Drop `console` statements
+          drop_console: true
+        },
+
+        // Mangling specific options
+        mangle: {
+          // Don't mangle $
+          except: ['$', 'webpackJsonp'],
+
+          // Don't care about IE8
+          screw_ie8 : true,
+
+          // Don't mangle function names
+          keep_fnames: true
+        }
+      })
+    ]
+  };
+};
+
+exports.setFreeVariable = function(key, value) {
+  const env = {};
+  env[key] = JSON.stringify(value);
+
+  return {
+    plugins: [
+      new webpack.DefinePlugin(env)
+    ]
+  };
+};
+
+exports.extractBundle = function(options) {
+  const entry = {};
+  entry[options.name] = options.entries;
+
+  return {
+    // Define an entry point needed for splitting.
+    entry: entry,
+    plugins: [
+      // Extract bundle and manifest files. Manifest is
+      // needed for reliable caching.
+      new webpack.optimize.CommonsChunkPlugin({
+        name: 'vendor',
+        minChunks: function(module) {
+          const userRequest = module.userRequest;
+
+          if (typeof userRequest !== 'string') {
+            return false;
+          }
+
+          // You can perform other similar checks here too.
+          // Now we check just node_modules.
+          return userRequest.indexOf('node_modules') >= 0;
+        }
+      })
+    ]
+  };
+};
+
+exports.clean = function(path) {
+  return {
+    plugins: [
+      new CleanWebpackPlugin([path], {
+        // Without `root` CleanWebpackPlugin won't point to our
+        // project and will fail to work.
+        root: process.cwd()
+      })
+    ]
+  };
+};
+
+exports.extractCSS = function(paths) {
+  return {
+    module: {
+      rules: [
+        // Extract CSS during build
+        {
+          test: /\.css$/,
+          loader: ExtractTextPlugin.extract({
+            fallbackLoader: 'style-loader',
+            loader: 'css-loader'
+          }),
+          include: paths
+        }
+      ]
+    },
+    plugins: [
+      // Output extracted CSS to a file
+      new ExtractTextPlugin('[name].css')
+    ]
+  };
+};
+
+
+exports.purifyCSS = function(paths) {
+  return {
+    plugins: [
+      new PurifyCSSPlugin({
+        basePath: process.cwd(),
+        // `paths` is used to point PurifyCSS to files not
+        // visible to Webpack. This expects glob patterns so
+        // we adapt here.
+        paths: paths.map(path => `${path}/*`),
+        // Walk through only html files within node_modules. It
+        // picks up .js files by default!
+        resolveExtensions: ['.html']
+      })
+    ]
+  };
+};
